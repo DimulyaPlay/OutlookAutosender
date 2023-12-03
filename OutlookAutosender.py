78e9212cc4cd3fc1a0625d526df355a0d85f51dd
DimulyaPlayHome <dimasiksosnin@mail.ru>
2023-12-03 11:23:51 +0500

    Changed ui to mainwindow better minimizing experience

--- OutlookAutosender.py
+++ OutlookAutosender.py
@@ -5,7 +5,7 @@ from main_functions import *
 from main_window import MainWindow
 
 # pyinstaller --noconfirm --onedir --console --icon "C:/Users/dimas/OtpravkaRosreestr/icons8-carrier-pigeon-64.ico" --add-data "C:/Users/dimas/OtpravkaRosreestr/UI;UI" "C:/Users/dimas/OtpravkaRosreestr/OutlookAutosender.py"
-# pyinstaller --noconfirm --onedir --console --windowed --icon "C:/Users/dimas/OtpravkaRosreestr/icons8-carrier-pigeon-64.ico" --add-data "C:/Users/dimas/OtpravkaRosreestr/UI;UI" "C:/Users/dimas/OtpravkaRosreestr/OutlookAutosender.py"
+# pyinstaller --noconfirm --onedir --console --windowed --icon "C:/Users/dimas/OtpravkaRosreestr/icons8-carrier-pigeon-64.ico" --add-data "C:/Users/dimas/OtpravkaRosreestr/UI;UI" --add-data "C:/Users/dimas/OtpravkaRosreestr/readme.txt;." "C:/Users/dimas/OtpravkaRosreestr/OutlookAutosender.py"
 
 
 if __name__ == '__main__':
