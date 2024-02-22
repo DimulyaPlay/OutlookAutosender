be1e764f6f8367233287044a81059051890d81eb
DimulyaPlay <dimasiksosnin@mail.ru>
2024-02-22 15:30:51 +0500

    schedule refactoring, gui improvements

--- main_window.py
+++ main_window.py
@@ -2,7 +2,7 @@ import sys
 import os
 import traceback
 from PyQt5.QtWidgets import QMainWindow, QLabel, QLineEdit, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, \
-    QCheckBox, QRadioButton, QGroupBox, QComboBox, QPlainTextEdit, QSpinBox, QTimeEdit, QToolButton, QFileDialog,\
+    QCheckBox, QGroupBox, QComboBox, QPlainTextEdit, QSpinBox, QTimeEdit, QToolButton, QFileDialog,\
     QPushButton, QApplication, QSystemTrayIcon, QAction, QMenu
 from PyQt5.QtGui import QIcon
 from PyQt5 import uic, QtCore
@@ -23,6 +23,9 @@ class MainWindow(QMainWindow):
         icon = QIcon("UI/icons8-carrier-pigeon-64.png")
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowMaximizeButtonHint)
         self.setWindowIcon(icon)
+        self.running = False
+        self.schedule_timers = []
+        self.timer = QTimer(self)  # Создание таймера
         def tray_activated(reason):
             if reason == QSystemTrayIcon.DoubleClick:
                 self.show_window()
@@ -58,12 +61,6 @@ class MainWindow(QMainWindow):
         toolButton_lineEdit_put_path.clicked.connect(lambda: self.set_user_dir('lineEdit_put_path'))
         toolButton_lineEdit_put_path = self.findChild(QToolButton, 'toolButton_lineEdit_csp_path')
         toolButton_lineEdit_put_path.clicked.connect(lambda: self.set_user_dir('lineEdit_csp_path'))
-        self.radioButton_periodic = self.findChild(QRadioButton, 'radioButton_periodic')
-        self.radioButton_periodic.setChecked(config['radioButton_periodic'])
-        self.radioButton_schedule = self.findChild(QRadioButton, 'radioButton_schedule')
-        self.radioButton_schedule.setChecked(config['radioButton_schedule'])
-        self.radioButton_periodic.toggled.connect(self.save_params)
-        self.radioButton_schedule.toggled.connect(self.save_params)
         self.comboBox_certs = self.findChild(QComboBox, 'comboBox_certs')
         cert_names = get_cert_names(os.path.join(self.config['lineEdit_csp_path'], 'certmgr.exe'))
         self.comboBox_certs.addItems(cert_names)
@@ -83,7 +80,7 @@ class MainWindow(QMainWindow):
         self.timeEdit_connecting_delay.setTime(QtCore.QTime.fromString(config['timeEdit_connecting_delay'], 'mm:ss'))
         self.timeEdit_connecting_delay.timeChanged.connect(self.save_params)
         self.start_scheduler = self.findChild(QPushButton, 'pushButton_start_stop_workers')
-        self.start_scheduler.clicked.connect(self.switch_tasker)
+        self.start_scheduler.clicked.connect(self.toggleScheduler)
         self.start_manual = self.findChild(QPushButton, 'pushButton_create_mail_now')
         self.start_manual.clicked.connect(lambda: self.send_mail_manual(True))
         pushButto_send_edo_manual = self.findChild(QPushButton, 'pushButto_send_edo_manual')
@@ -98,14 +95,13 @@ class MainWindow(QMainWindow):
         pushButton_edo.clicked.connect(self.open_edo_settings)
         autorun = self.findChild(QCheckBox, 'checkBox_autorun')
         autorun.clicked.connect(add_to_startup)
-        # Создание обработчика событий
         self.event_handler = MyHandler(self)
         self.observer = Observer()
         self.directory_to_watch = self.config['lineedit_input_edo']
         self.update_interval = 1000
         self.observer.schedule(self.event_handler, path=self.directory_to_watch, recursive=False)
         self.autostart_timer = QTimer(self)
-        self.autostart_timer.timeout.connect(self.switch_tasker)
+        self.autostart_timer.timeout.connect(self.toggleScheduler)
         if config['checkBox_autosend_edo']:
             self.toggle_edo_autosender(2)
         if config['checkBox_autorun'] and config['checkBox_autostart']:
@@ -117,6 +113,53 @@ class MainWindow(QMainWindow):
             self.autostart_timer.start(secs * 1000)
         else:
             self.show()
+
+    def toggleScheduler(self):
+        if self.running:
+            self.stopScheduler()  # Остановка всех таймеров
+            self.start_scheduler.setText('Запустить работу по расписанию')
+            self.running = False
+        else:
+            self.startScheduler()  # Запуск диспетчера
+            self.start_scheduler.setText('Остановить работу по расписанию')
+            self.running = True
+
+    def startScheduler(self):
+        errors = self.check_fields(False)
+        if errors:
+            self.add_log_message('\n'.join(errors))
+            return
+        try:
+            if self.config['checkBox_periodic']:
+                hours, minutes = self.timeEdit_send_period.time().toString('HH:mm').split(':')
+                total_seconds = (int(hours) * 3600 + int(minutes) * 60) * 1000
+                self.timer.start(total_seconds)
+                self.timer.timeout.connect(self.send_mail_manual)
+                self.add_log_message(f'Периодический таймер запущен, запуск задачи каждые {total_seconds} секунд.')
+            if self.config['checkBox_schedule']:
+                schedule_times = self.config['lineEdit_schedule'].split(',')
+                for time_str in schedule_times:
+                    timer = QTimer(self)
+                    time = QTime.fromString(time_str, "HH:mm")
+                    now = QTime.currentTime()
+                    if now < time:
+                        milliseconds = now.msecsTo(time)
+                    else:
+                        milliseconds = 86400000 - now.msecsTo(time)  # Перезапуск на следующий день
+                    timer.singleShot(milliseconds, self.send_mail_manual)
+                    self.schedule_timers.append(timer)  # Сохраняем таймеры, чтобы они не удалялись
+                    self.add_log_message(f'Таймер на {time_str} запущен.')
+            self.add_log_message('Работа по расписанию запущена.')
+        except Exception as e:
+            self.add_log_message(f'Ошибка при запуске таймеров: {e}')
+
+    def stopScheduler(self):
+        # Остановка всех таймеров
+        self.timer.stop()  # Остановка периодического таймера
+        for timer in self.schedule_timers:
+            timer.stop()  # Остановка таймеров по расписанию
+        self.schedule_timers.clear()  # Очистка списка таймеров по расписанию
+        self.add_log_message('Работа по расписанию была остановлена пользователем.')
 
     def hideEvent(self, event):
         event.ignore()
@@ -162,11 +205,11 @@ class MainWindow(QMainWindow):
         self.config['spinBox_max_attach_weight'] = self.spinBox_max_attach_weight.value()
         self.config['timeEdit_send_period'] = self.timeEdit_send_period.time().toString('HH:mm')
         self.config['timeEdit_connecting_delay'] = self.timeEdit_connecting_delay.time().toString('mm:ss')
-        self.config['radioButton_periodic'] = self.radioButton_periodic.isChecked()
-        self.config['radioButton_schedule'] = self.radioButton_schedule.isChecked()
+        self.config['checkBox_periodic'] = self.checkBox_periodic.isChecked()
+        self.config['checkBox_schedule'] = self.checkBox_schedule.isChecked()
         save_config(config_file, self.config)
 
-    def send_mail_manual(self, manual):
+    def send_mail_manual(self, manual=False):
         errors = self.check_fields(manual)
         if errors:
             self.add_log_message('\n'.join(errors))
@@ -188,82 +231,6 @@ class MainWindow(QMainWindow):
                 self.add_log_message(traceback_str)
                 traceback.print_exc()
 
-    def switch_tasker(self):
-        if self.autostart_timer.isActive():
-            self.autostart_timer.stop()
-        errors = self.check_fields()
-        if errors:
-            self.add_log_message('\n'.join(errors))
-            return
-        rb1 = self.findChild(QRadioButton, 'radioButton_periodic')
-        rb1.setEnabled(self.daemon_running)
-        rb2 = self.findChild(QRadioButton, 'radioButton_schedule')
-        rb2.setEnabled(self.daemon_running)
-        if self.config['radioButton_periodic']:
-            self.switch_periodic()
-        elif self.config['radioButton_schedule']:
-            self.switch_scheduled()
-
-    def handle_timer_timeout(self):
-        self.send_mail_manual(False)
-        selected_times = [QTime.fromString(i, 'HH:mm') for i in self.config['lineEdit_schedule'].split(',')]
-        current_datetime = QDateTime.currentDateTime()
-        current_time = current_datetime.time()
-        next_run_times = [self.calculate_time_difference(current_time, time) for time in selected_times]
-        min_time_difference = min(next_run_times)
-        self.timer_interval = min_time_difference
-        self.timer.setInterval(self.timer_interval)
-
-    @staticmethod
-    def calculate_time_difference(current_time, selected_time):
-        next_run_datetime = QDateTime.currentDateTime()
-        next_run_datetime.setTime(selected_time)
-        current_date = QDateTime.currentDateTime().date()
-        next_run_datetime.setDate(current_date)
-        if next_run_datetime < QDateTime.currentDateTime():
-            next_run_datetime = next_run_datetime.addDays(1)
-        time_difference = QDateTime.currentDateTime().msecsTo(next_run_datetime)
-        return time_difference
-
-    def switch_scheduled(self):
-        if not self.daemon_running:
-            self.timer = QTimer(self)
-            self.timer.timeout.connect(self.handle_timer_timeout)
-            selected_times = [QTime.fromString(i, 'HH:mm') for i in self.config['lineEdit_schedule'].split(',')]
-            current_datetime = QDateTime.currentDateTime()
-            current_time = current_datetime.time()
-            next_run_times = [self.calculate_time_difference(current_time, time) for time in selected_times]
-            min_time_difference = min(next_run_times)
-            self.timer_interval = min_time_difference
-            self.timer.setInterval(self.timer_interval)
-            self.timer.start()
-            self.daemon_running = True
-            self.start_scheduler.setText('Остановить работу')
-            self.add_log_message('Задача запустится в заданные временные точки')
-        else:
-            self.daemon_running = False
-            self.timer.stop()
-            self.start_scheduler.setText('Запустить работу в выбранном режиме')
-            self.add_log_message('Задача остановлена пользователем')
-
-    def switch_periodic(self):
-        if not self.daemon_running:
-            self.timer = QTimer(self)
-            self.timer.timeout.connect(lambda: self.send_mail_manual(False))
-            hours, minutes = self.timeEdit_send_period.time().toString('HH:mm').split(':')
-            total_seconds = (int(hours) * 3600 + int(minutes) * 60) * 1000
-            self.timer_interval = total_seconds
-            self.timer.setInterval(self.timer_interval)
-            self.daemon_running = True
-            self.send_mail_manual(False)
-            self.add_log_message('Задача запущена')
-            self.timer.start()
-            self.start_scheduler.setText('Остановить работу')
-        else:
-            self.daemon_running = False
-            self.start_scheduler.setText('Запустить работу в выбранном режиме')
-            self.timer.stop()
-            self.add_log_message('Задача остановлена пользователем')
 
     def add_log_message(self, message):
         current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -294,7 +261,7 @@ class MainWindow(QMainWindow):
                 errors.append('Не обнаружен валидный email адрес')
         else:
             errors.append('Должен быть указан хотя бы один адресат')
-        if self.config['radioButton_schedule'] and not manual:
+        if self.config['checkBox_schedule'] and not manual:
             valid = True
             periods = self.config['lineEdit_schedule'].split(',')
             for period in periods:
